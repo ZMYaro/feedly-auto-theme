f69fe6862d0b7b061bc26e2b7fa0dbb76a9a2511
Zachary Yaro <zmyaro@zmyaro.com>
2020-10-08 15:40:55 -0400

    Initial commit with first extension version

---
+++ extension/content_script.js
@@ -0,0 +1,22 @@
+'use strict';
+
+function setOverrideTheme() {
+	// Add the `theme--dark` or `theme--light` class to the <body>.
+	if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+		document.body.classList.add('theme--dark');
+		document.body.classList.remove('theme--light');
+		return;
+	} else if (window.matchMedia('(prefers-color-scheme: light)').matches) {
+		document.body.classList.add('theme--light');
+		document.body.classList.remove('theme--dark');
+		return;
+	}
+	// If `prefers-color-scheme` matches neither, do nothing.
+}
+
+// Set listeners to update the theme if the user's system theme preference changes.
+window.matchMedia('(prefers-color-scheme: dark)').addListener(setOverrideTheme);
+window.matchMedia('(prefers-color-scheme: light)').addListener(setOverrideTheme);
+
+// Set to the system theme at load time.
+setOverrideTheme();
