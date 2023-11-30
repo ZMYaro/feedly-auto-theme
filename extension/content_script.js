02d30a42a9badad0a083f962608269eef8716073
Zachary Yaro <zmyaro@zmyaro.com>
2023-11-30 15:03:33 -0500

    Refactor the extension to use a mutation observer to detect when Feedly changes the theme

--- extension/content_script.js
+++ extension/content_script.js
@@ -1,26 +1,32 @@
 'use strict';
 
+var DARK_THEME_CLASS = 'theme--dark',
+	LIGHT_THEME_CLASS = 'theme--light';
+
 function setOverrideTheme() {
 	// Add the `theme--dark` or `theme--light` class to the <body>.
-	if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
-		document.body.classList.add('theme--dark');
-		document.body.classList.remove('theme--light');
+	if (window.matchMedia('(prefers-color-scheme: dark)').matches && !document.body.classList.contains(DARK_THEME_CLASS)) {
+		document.body.classList.add(DARK_THEME_CLASS);
+		document.body.classList.remove(LIGHT_THEME_CLASS);
 		return;
-	} else if (window.matchMedia('(prefers-color-scheme: light)').matches) {
-		document.body.classList.add('theme--light');
-		document.body.classList.remove('theme--dark');
+	} else if (window.matchMedia('(prefers-color-scheme: light)').matches && !document.body.classList.contains(LIGHT_THEME_CLASS)) {
+		document.body.classList.add(LIGHT_THEME_CLASS);
+		document.body.classList.remove(DARK_THEME_CLASS);
 		return;
 	}
 	// If `prefers-color-scheme` matches neither, do nothing.
 }
 
-// Set listeners to update the theme if the user's system theme preference changes.
+// Set to the system theme at load time.
+setOverrideTheme();
+
+// Update the theme if the user's system theme preference changes.
 window.matchMedia('(prefers-color-scheme: dark)').addListener(setOverrideTheme);
 window.matchMedia('(prefers-color-scheme: light)').addListener(setOverrideTheme);
 
-// Set to the system theme at load time.
-setOverrideTheme();
-// Set it again after load in case Feedly changes it on load.
-window.addEventListener('load', function () {
-	setTimeout(setOverrideTheme, 10);
+// Set the theme again if Feedly changes it.
+var observer = new MutationObserver(setOverrideTheme);
+observer.observe(document.body, {
+	attributes: true,
+	attributeFilter: ['class']
 });
